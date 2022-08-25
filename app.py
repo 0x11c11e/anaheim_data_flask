4d7181d232dd3edbd6b484653b7ef5ce91e86c82
0X11c11e <0x11c11e@protonmail.com>
2022-08-25 11:56:41 -0700

    changes

--- app.py
+++ app.py
@@ -7,10 +7,6 @@ from flask_session import Session
 from flask import (Flask, render_template)
 
 app = Flask(__name__)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
 r = {}
 
 
@@ -38,7 +34,7 @@ def search_voters(street_name):
     voter_ids = list(r[street_name_hash].keys())
     for voter_id in voter_ids:
         voters[voter_id] = r[street_name_hash][voter_id]
-    session['street_name_hash'] = street_name_hash
+
     dt = str(datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
 
     return render_template('search_voters.html', voters=voters, voter_ids=voter_ids, dt=dt, street_name=street_name)
