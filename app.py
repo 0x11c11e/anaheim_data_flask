4ecc77ddcd315cfd619f0ef2f25214b1a45cbdaa
0X11c11e <0x11c11e@protonmail.com>
2022-08-25 10:59:28 -0700

    new changes

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 import hashlib
-import json
 import pickle
-import re
-import shelve
 from datetime import datetime
+from flask import Flask, session
+from flask_session import Session
 
-import boto3
-from boto3.dynamodb.conditions import Attr, Key
-from flask import (Flask, flash, json, jsonify, redirect, render_template,
-                   request, url_for)
-import urllib.request as urllib2
+from flask import (Flask, render_template)
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 r = {}
 
@@ -29,7 +27,7 @@ def search_streets(streetname):
     for line in file.readlines():
         if streetname in line:
             streets.append(line)
-
+    
     return render_template('results_streets.html', streets=streets, md5_hash=md5_hash, results=len(streets))
 
 
@@ -40,12 +38,20 @@ def search_voters(street_name):
     voter_ids = list(r[street_name_hash].keys())
     for voter_id in voter_ids:
         voters[voter_id] = r[street_name_hash][voter_id]
-
+    session['street_name_hash'] = street_name_hash
     dt = str(datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
 
     return render_template('search_voters.html', voters=voters, voter_ids=voter_ids, dt=dt, street_name=street_name)
 
 
+@app.route('/search/voter/<string:voter_id>', methods=['GET'])
+def search_voter(street_name, voter_id):
+
+    street_name_hash = session.get('street_name_hash', 'not set')
+    if street_name_hash == 'not set':
+        return render_template('error.html', error='No street name hash set')
+    return render_template('voter.html', voter=r[street_name_hash][voter_id])
+    
 def md5_hash(string):
 
     return str(hashlib.md5(string.encode('utf-8')).hexdigest())
